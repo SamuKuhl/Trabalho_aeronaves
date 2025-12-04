4551e5b34d7198ed0074f65e8898271e006346d6
Samuel Kühl <124585880+SamuKuhl@users.noreply.github.com>
2025-12-04 06:38:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "aeronaves.h"
 #include "rotas.h"
 #include "utils.h"
 
-int main() {
-    inicializarAeronaves();
-    inicializarRotas();
-
-    int opcao;
-
-    do {
-        printf("\n=== MENU ===\n");
-        printf("1 - Cadastrar aeronave\n");
-        printf("2 - Listar aeronaves\n");
-        printf("0 - Sair\n");
-        printf("Escolha: ");
-
-        scanf("%d", &opcao);
-        getchar();
-
-        switch (opcao) {
-            case 1: cadastrarAeronave(); break;
-            case 2: listarAeronaves(); break;
+void menuAeronaves(Aeronave **lista){
+    int op;
+    do{
+        printf("\n--- MENU AERONAVES ---\n");
+        printf("1. Cadastrar aeronave\n");
+        printf("2. Listar aeronaves\n");
+        printf("3. Salvar binário\n");
+        printf("4. Carregar binário\n");
+        printf("0. Voltar\n> ");
+        scanf("%d",&op); getchar();
+        switch(op){
+            case 1: cadastrarAeronave(lista); break;
+            case 2: listarAeronaves(*lista); break;
+            case 3: salvarAeronaves(*lista); break;
+            case 4: carregarAeronaves(lista); break;
         }
-
-    } while (opcao != 0);
-
-    return 0;
+    }while(op!=0);
 }
-#include <stdio.h>
-#include "aeronaves.h"
-#include "rotas.h"
-#include "utils.h"
 
-int main() {
-    inicializarAeronaves();
-    inicializarRotas();
-
-    int opcao;
-
-    do {
-        printf("\n=== MENU ===\n");
-        printf("1 - Cadastrar aeronave\n");
-        printf("2 - Listar aeronaves\n");
-        printf("0 - Sair\n");
-        printf("Escolha: ");
-
-        scanf("%d", &opcao);
-        getchar();
-
-        switch (opcao) {
-            case 1: cadastrarAeronave(); break;
-            case 2: listarAeronaves(); break;
+void menuRotas(Rota **lista){
+    int op;
+    do{
+        printf("\n--- MENU ROTAS ---\n");
+        printf("1. Cadastrar rota\n");
+        printf("2. Listar rotas\n");
+        printf("3. Salvar binário\n");
+        printf("4. Carregar binário\n");
+        printf("0. Voltar\n> ");
+        scanf("%d",&op); getchar();
+        switch(op){
+            case 1: cadastrarRota(lista); break;
+            case 2: listarRotas(*lista); break;
+            case 3: salvarRotas(*lista); break;
+            case 4: carregarRotas(lista); break;
         }
+    }while(op!=0);
+}
 
-    } while (opcao != 0);
-
+int main(){
+    Aeronave *listaA = NULL;
+    Rota *listaR = NULL;
+    int op;
+    do{
+        printf("\n=== SISTEMA FROTA AÉREA ===\n");
+        printf("1. Aeronaves\n");
+        printf("2. Rotas\n");
+        printf("0. Sair\n> ");
+        scanf("%d",&op); getchar();
+        if(op==1) menuAeronaves(&listaA);
+        if(op==2) menuRotas(&listaR);
+    }while(op!=0);
     return 0;
 }
