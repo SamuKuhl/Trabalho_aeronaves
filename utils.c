5b2a9f4c710c4424d5d779b8b0b0563bbd24ce3c
Samuel Kühl <124585880+SamuKuhl@users.noreply.github.com>
2025-12-04 06:39:31 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include "utils.h"
-
-void lerString(char *str, int tamanho) {
-    fgets(str, tamanho, stdin);
-    str[strcspn(str, "\n")] = '\0';
-}
-
-void limparTela(){
-
-}
-
-void pausar (){
-    
-}
+void dummy(){}
