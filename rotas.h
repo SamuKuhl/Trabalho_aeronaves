6919843b1d6ebec4508cf8cd77b577d30f725822
Samuel Kühl <124585880+SamuKuhl@users.noreply.github.com>
2025-12-04 06:39:12 -0300

    Update rotas.h

--- rotas.h
+++ rotas.h
@@ -1,8 +1,23 @@
 #ifndef ROTAS_H
 #define ROTAS_H
 
-void inicializarRotas();
-void cadastrarRotas();
-void listarRotas();
+typedef struct rota{
+    char codigo[20];         
+    char dataHora[20];        
+    char origem[50];       
+    char destino[50];       
+    float tempoVoo;         
+    float combustivel;        
+    int passageiros;          
+    float carga;             
+    char tripulacao[100];     
+    char aeronaveAlocada[20];  
+    struct rota *prox;
+}Rota;
+
+void cadastrarRota(Rota **lista);
+void listarRotas(Rota *lista);
+void salvarRotas(Rota *lista);
+void carregarRotas(Rota **lista);
 
 #endif
